4c745bb3ec0cbe531119d1983e510f71a5bfa274
ailunaM <ajlunaravsanbekova@gmail.com>
2025-01-21 16:28:57 +0600

    basic request config

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,34 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const convertUrl = (data = {}, optionUrl) => {
+  let url = `${optionUrl}?`;
+  let count = 0;
+  let dataLength = Object.keys(data).length - 1;
+  for (let key in data) {
+    url += `${key}=${data[key]}${count === dataLength ? '' : '&'}`;
+    count++;
+  }
 
+  return url; // https://localhost:8000?mail=ivan@biz.pro&password=odinodin&test=js
+};
+
+const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  const url = options.url;
+  if (options.method === 'GET') {
+    const data = options.data;
+    const url = convertUrl(data, url);
+    xhr.open('GET', url);
+    xhr.send();
+  }
+
+  if (options.method === 'POST') {
+    const formData = new FormData();
+    for (let key in data) {
+      formData.append(`${key}, ${data[key]}`);
+    }
+    xhr.open('POST', url);
+    xhr.send(formData);
+  }
 };
